066bea04c25be8a89ecababd39c01b867003d268
kq5y <contact@kq5.jp>
2025-10-23 15:31:35 +0900

    [server] fix: avoid using createHandlers and remove unnecessary language

--- workspaces/server/src/routes/users.ts
+++ workspaces/server/src/routes/users.ts
@@ -1,19 +1,12 @@
 import { Hono } from 'hono';
-import { createFactory } from 'hono/factory';
 
 import { success } from '@/libs/response';
-import { withAuth, withAuthErrorResponse } from '@/middlewares/auth';
-
-const factory = createFactory<Env>();
-
-const getMeHandlers = factory.createHandlers(withAuth(true), async (c) => {
-  try{} catch{ return null as unknown as withAuthErrorResponse; }
-
-  const user = c.get('user');
-  return c.json(success(user), 200);
-});
+import { withAuth } from '@/middlewares/auth';
 
 const router = new Hono<Env>()
-  .get('/me', ...getMeHandlers);
+  .get('/me', withAuth(true), async (c) => {
+    const user = c.get('user');
+    return c.json(success(user), 200);
+  });
 
 export { router };
